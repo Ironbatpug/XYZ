31cbabb938684959f29c02333f4793e070d8b5ee
Ironbatpug <molnarcsaba92@gmail.com>
2019-03-25 20:31:29 +0100

    innitial commit

---
+++ XYZ/Controller/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  XYZ
+//
+//  Created by Molnár Csaba on 2019. 03. 23..
+//  Copyright © 2019. Molnár Csaba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
