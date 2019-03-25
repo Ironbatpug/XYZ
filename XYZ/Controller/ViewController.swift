acb00b671dcf48bd94c552d156ef7646a8d2f374
Ironbatpug <molnarcsaba92@gmail.com>
2019-03-25 20:59:23 +0100

    lesson-02 AcceloremeterData

--- XYZ/Controller/ViewController.swift
+++ XYZ/Controller/ViewController.swift
@@ -7,15 +7,27 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var xLabel: UILabel!
     @IBOutlet weak var yLabel: UILabel!
     @IBOutlet weak var zLabel: UILabel!
     
+    var motionManager: CMMotionManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        motionManager = CMMotionManager()
+        motionManager.startAccelerometerUpdates(to: .main, withHandler: updateLabels)
     }
+    
+        func updateLabels(data: CMAccelerometerData?,error:  Error?) {
+            guard  let accelometerData = data else { return}
+            print(accelometerData)
+            }
 }
+
 
